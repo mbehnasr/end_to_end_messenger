49cc8c21f215b7e8ebcfceca3d5903d9377a9e56
m.m.behnasr.2000 <m.m.behnasr.2000@gmail.com>
2023-06-29 20:07:34 +0330

    change

--- c.py
+++ c.py
@@ -2,17 +2,27 @@
 
 import asyncio
 import websockets
+import uuid
+import json
+client_uuid = str(uuid.uuid4())
 
 async def hello():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
+        while True:
 
-        await websocket.send(name)
-        print(f">>> {name}")
+            name = input("What's your name? ")
 
-        greeting = await websocket.recv()
-        print(f"<<< {greeting}")
+            data = {
+                'userId': name+client_uuid,
+                'uuid': client_uuid,
+            }
+            data_string = json.dumps(data)
+            await websocket.send(data_string)
+            print(f">>> {name}")
+
+            greeting = await websocket.recv()
+            print(f"<<< {greeting}")    
 
 if __name__ == "__main__":
-    asyncio.run(hello())
+    asyncio.run(hello()) 
--- s.py
+++ s.py
@@ -4,15 +4,54 @@ import asyncio
 import datetime
 import random
 import websockets
+import json
+import os
+import logging
+# logger = logging.getLogger('websockets')
+# logger.setLevel(logging.DEBUG)
+# logger.addHandler(logging.StreamHandler())
+
+
 
 CONNECTIONS = set()
+USERS = "users.json"
+async def save_user(user):
+    try:
+        """ new_user = {
+            [user[uuid]]: user[userId]
+        }
+
+        users = json.dumps(USERS)
+        users.append(new_user)
+
+        with open(USERS, "w") as file:
+            json.dump(users, file, indent=4)
+ """
+        print("omad tosh")
+        print(f"Received and saved file: {userId}")
+    except json.JSONDecodeError as e:
+        print(f"Invalid JSON format: {e}")
+
+
+
 
 async def register(websocket):
+    print(websocket)
+    print("\n")
+    print(id(websocket))
     CONNECTIONS.add(websocket)
+    print(CONNECTIONS)
     try:
+        new_user = await websockets.recv()
+        new_user = json.loads(new_user)
+        print(new_user)
+        await register(new_user["uuid"])
         await websocket.wait_closed()
     finally:
         CONNECTIONS.remove(websocket)
+
+
+
 
 async def show_time():
     while True:
@@ -20,8 +59,10 @@ async def show_time():
         websockets.broadcast(CONNECTIONS, message)
         await asyncio.sleep(random.random() * 2 + 1)
 
+
+
 async def main():
-    async with websockets.serve(register, "localhost", 5678):
+    async with websockets.serve(register, "localhost", 8765):
         await show_time()
 
 if __name__ == "__main__":
