5038af4219caf632e68fd138aff89808f15cf207
m.m.behnasr.2000 <m.m.behnasr.2000@gmail.com>
2023-07-01 11:24:56 +0330

    update client and server

--- c.py
+++ c.py
@@ -6,23 +6,78 @@ import uuid
 import json
 client_uuid = str(uuid.uuid4())
 
+from Crypto.PublicKey import RSA
+key = RSA.generate(2048)
+private_key = key.export_key()
+public_key = key.publickey().export_key()
+
+
+"""
+create session key for client chat with each other
+from Crypto.Cipher import AES
+
+file_in = open("encrypted.bin", "rb")
+nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
+file_in.close()
+
+# let's assume that the key is somehow available again
+cipher = AES.new(key, AES.MODE_EAX, nonce)
+data = cipher.decrypt_and_verify(ciphertext, tag)
+ """
+
+"""
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(2048)
+private_key = key.export_key()
+file_out = open("private.pem", "wb")
+file_out.write(private_key)
+file_out.close()
+
+public_key = key.publickey().export_key()
+file_out = open("receiver.pem", "wb")
+file_out.write(public_key)
+file_out.close()
+#Beine in file va file baadi byd amaliate tabadole session key anjam she. """
+
+
 async def hello():
     uri = "ws://localhost:8765"
+
     async with websockets.connect(uri) as websocket:
+
+        data = {
+            'userId': client_uuid,
+            'uuid': client_uuid,
+        }
+        data_string = json.dumps(data)
+
+        await websocket.send(data_string)
+        send_okaye = await websocket.recv()
+        users = await websocket.recv()
+        users_json =  json.loads(users)
+
+
+        for user in users_json:
+            keys = user.keys()
+            for key in keys:
+                print(key)
+
+
         while True:
+            target_user = input("\ninput target user:\n")
+            print(f">>> {target_user}")
+            message = {}
+            message["type"] = "send_message"
+            message["target_user"] = target_user
+            message["message"] = "I wana connect to another person"
+            message["public_key"] = public_key
+            type(public_key)
 
-            name = input("What's your name? ")
+            await websocket.send(json.dumps({"type":"send_message","target_user":target_user,"message":"I wana connect to another person","public_key":str(public_key)}))
 
-            data = {
-                'userId': name+client_uuid,
-                'uuid': client_uuid,
-            }
-            data_string = json.dumps(data)
-            await websocket.send(data_string)
-            print(f">>> {name}")
-
-            greeting = await websocket.recv()
-            print(f"<<< {greeting}")    
+            answer = await websocket.recv()
+            print(f"<<< {answer}")
 
 async def main():
     async with websockets.serve(hello, "localhost", 8765):
--- s.py
+++ s.py
@@ -7,32 +7,70 @@ import websockets
 import json
 import os
 import logging
+
+
+#Beine in file va file baadi byd amaliate tabadole session key anjam she.
 # logger = logging.getLogger('websockets')
 # logger.setLevel(logging.DEBUG)
 # logger.addHandler(logging.StreamHandler())
+"""
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
-users=[]
+data = b'secret data'
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+file_out = open("encrypted.bin", "wb")
+[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+file_out.close()
+ """
+
+"""
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(2048)
+private_key = key.export_key()
+file_out = open("private.pem", "wb")
+file_out.write(private_key)
+file_out.close()
+
+public_key = key.publickey().export_key()
+file_out = open("receiver.pem", "wb")
+file_out.write(public_key)
+file_out.close()
+#Beine in file va file baadi byd amaliate tabadole session key anjam she.
+"""
 
 CONNECTIONS = set()
-# USERS = "./users.json"
+connected_clients = {}
+
+SEND_MESSAGE_TO_ANOTHER = "send_message"
+TARGET_USER = "target_user"
+
+
 async def save_user(user, websocket):
-    global USERS
     try:
         new_user = {
             user["uuid"]: str(websocket),
         }
+        connected_clients[user["uuid"]] = websocket
 
         with open('./users.json') as file:
-            json_data = json.load(file)
+            json_users = json.load(file)
+        json_users_with_new_user = json_users
+        json_users_with_new_user.append(new_user)
 
-        json_data.append(new_user)
-        print(json_data)
+
         with open('users.json', 'w') as file:
-               json.dump(json_data, file)
+               json.dump(json_users_with_new_user, file)
 #         await asyncio.sleep(random.random() * 2 + 10)
-        websockets.broadcast(CONNECTIONS, json.dumps(json_data.append({"type": "users"})))
-
-        print(f"Received and saved file: {new_user}")
+#         websockets.broadcast(CONNECTIONS, json.dumps(json_data.append({"type": "users"})))
+        for socket in CONNECTIONS:
+            await socket.send("is connected")
+            await socket.send(json.dumps(json_users_with_new_user))
     except json.JSONDecodeError as e:
         print(f"Invalid JSON format: {e}")
 
@@ -40,36 +78,77 @@ async def save_user(user, websocket):
 
 
 async def register(websocket):
-    print(websocket)
-    print("\n")
-    print(id(websocket))
+
     CONNECTIONS.add(websocket)
-    print(CONNECTIONS)
+    client_id = id(websocket)
+    connected_clients[client_id] = websocket
+
     new_user = await websocket.recv()
     new_user = json.loads(new_user)
-    print(new_user["uuid"])
+#     print(new_user["uuid"])
     await save_user(new_user , websocket)
 
-    try:
 
+    await communicate(websocket)
+    try:
         await websocket.wait_closed()
     finally:
         CONNECTIONS.remove(websocket)
+        connected_clients.pop(client_id, None)
 
 
 
 
-async def show_time():
+async def communicate(websocket):
     while True:
-        message = datetime.datetime.utcnow().isoformat() + "Z"
-        websockets.broadcast(CONNECTIONS, message)
+
+        message = await websocket.recv()
+#         print(message)
+        message_json = json.loads(message)
+        if(message_json["type"] == SEND_MESSAGE_TO_ANOTHER):
+            """ with open('./users.json') as file:
+                json_users = json.load(file)
+            print(type(json_users))
+            print(json_users)
+            print(connected_clients[message_json["target_user"]]) """
+            websocket_target_user = connected_clients.get(message_json["target_user"])
+            print(connected_clients)
+
+            await websocket_target_user.send("fuckme so hard")
+
+
+#         message = datetime.datetime.utcnow().isoformat() + "Z"
+#         websockets.broadcast(CONNECTIONS, message)
         await asyncio.sleep(random.random() * 2 + 1)
 
 
 
-async def main():
-    async with websockets.serve(register, "localhost", 8765):
-        await show_time()
+"""
+async def say_hello():
+    while True:
+        for socket in CONNECTIONS:
+            socket.send("hello")
 
-if __name__ == "__main__":
-    asyncio.run(main())
+"""
+
+
+
+
+# async def main():
+#     async with websockets.serve(register, "localhost", 8765):
+start_server = websockets.serve(register, 'localhost', 8765)
+loop = asyncio.get_event_loop()
+
+
+try:
+    loop.run_until_complete(start_server)
+    loop.run_forever()
+finally:
+    loop.close()
+# asyncio.get_event_loop().run_until_complete(start_server)
+# asyncio.get_event_loop().run_forever()
+#         await show_time()
+#         await say_hello()
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
